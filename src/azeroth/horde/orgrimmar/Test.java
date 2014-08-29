19e6d7a817ec812cb6b0c97292451b7597ef25c2
chris.zhang <xh1122@126.com>
2014-08-29 14:00:55 +0800

    init

---
+++ src/azeroth/horde/orgrimmar/Test.java
@@ -0,0 +1,16 @@
+package azeroth.horde.orgrimmar;
+
+public class Test {
+
+	public Test() {
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("Lok'tar ogar");
+	}
+
+}
