b4490954e6002044133f577602a80ce45593c50f
chris.zhang <xh1122@126.com>
2014-09-02 17:27:55 +0800

    effective java

---
+++ src/azeroth/effectivejava/MainEntry.java
@@ -0,0 +1,15 @@
+package azeroth.effectivejava;
+
+import azeroth.Utils;
+
+/**
+ * main method
+ * @author chris.zhang
+ *
+ */
+public class MainEntry {
+	public static void main(String[] args) {
+		Utils.print("Hello,world!");
+	}
+
+}
